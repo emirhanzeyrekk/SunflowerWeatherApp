7b3da7ca45e37083a287b4731faba0515422751b
Emirhan Zeyrek <emirhanzeyrekcom@gmail.com>
2024-05-08 16:04:46 +0300

    Initial Commit

---
+++ SunflowerWeather/SunflowerWeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  SunflowerWeatherApp.swift
+//  SunflowerWeather
+//
+//  Created by Emirhan Zeyrek on 8.05.2024.
+//
+
+import SwiftUI
+
+@main
+struct SunflowerWeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
